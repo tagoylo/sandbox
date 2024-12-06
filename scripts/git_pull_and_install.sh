783830d28fa7ef99ee6f59b9aa51445038dd529d
Trecia Agoylo <trecia.agoylo@analog.com>
2024-12-06 11:07:12 +0800

    add path where to clone
    Signed-off-by: Trecia Agoylo <trecia.agoylo@analog.com>

--- scripts/git_pull_and_install.sh
+++ scripts/git_pull_and_install.sh
@@ -18,6 +18,9 @@ REPO_URL="$1"
 BRANCH_NAME="${2:-main}"  # Default to 'main' if no branch specified
 REPO_NAME=$(basename "$REPO_URL" .git)
 
+# Change to the desired directory
+cd /root/do-not-delete-scripts
+
 # Pull the repository
 if [ -d "$REPO_NAME" ]; then
     echo "Repository '$REPO_NAME' already exists. Pulling latest changes..."
